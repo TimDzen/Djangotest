190afc9d3ec33784fbf2966d9532ad5505b19d5e
TimDzen <140600844+TimDzen@users.noreply.github.com>
2023-11-23 19:06:41 +0300

    1

--- djangorest/djangorest/urls.py
+++ djangorest/djangorest/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for djangorest project.
+URL configuration for djangotesting project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("api.urls")),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api-token-auth/', obtain_auth_token)
+
 ]
