5b388f488e182f10aa4d0e2ca67052d045db8511
TimDzen <140600844+TimDzen@users.noreply.github.com>
2023-11-28 10:38:05 +0300

    1

--- api/views.py
+++ api/views.py
@@ -25,7 +25,11 @@ class ProductViewSet(viewsets.ModelViewSet):
                           CanRetrieveProduct
                          ]
 
-    @action(detail=True, methods=['post'], url_path='supply', permission_classes=[IsSupplier])
+    @action(detail=True, 
+            methods=['post'], 
+            url_path='supply', 
+            permission_classes=[IsSupplier]
+           )
     def supply_product(self, request, pk=None):
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=True)
@@ -34,11 +38,18 @@ class ProductViewSet(viewsets.ModelViewSet):
 
         return Response(serializer.data)
 
-    @action(detail=True, methods=['post'], url_path='retrieve', permission_classes=[IsConsumer])
+    @action(detail=True,
+            methods=['post'],
+            url_path='retrieve',
+            permission_classes=[IsConsumer]
+           )
     def retrieve_product(self, request, pk=None):
         instance = self.get_object()
 
-        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer = self.get_serializer(instance,
+                                         data=request.data, 
+                                         partial=True
+                                        )
         serializer.is_valid(raise_exception=True)
         serializer.save()
 
